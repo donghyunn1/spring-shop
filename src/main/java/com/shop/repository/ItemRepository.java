c4474a8fd4a662e28260604fe21ccba7dc045265
donghyunn1 <dongdong0516@naver.com>
2024-09-14 17:49:43 +0900

    feat: 상품 repository 데이터 조회 구현

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,5 +3,12 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail); // 상품명과 상품 상세 설명 이용하여 조회
+    List<Item> findByPriceLessThan(Integer price); // price 변수보다 작은 상품 조회
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); // 상품의 가격이 높은 순으로 조회
 }
