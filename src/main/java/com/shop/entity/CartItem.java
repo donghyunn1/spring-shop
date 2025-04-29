ea395ba1579720188710de93769a2f48aeceea8d
양동현 <dongdong0516@naver.com>
2025-04-29 23:09:17 +0900

    feat: 지연 로딩 적용

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -14,11 +14,11 @@ public class CartItem {
     @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
     private Cart cart;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
