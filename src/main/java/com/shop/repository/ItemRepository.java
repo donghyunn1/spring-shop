fe2d6fdefb37bec74dccdd6132ab02cdd235a74c
양동현 <dongdong0516@naver.com>
2025-05-03 15:47:45 +0900

    feat: 상품 관리 기능 구현

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>,
-        QuerydslPredicateExecutor<Item> {
+        QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
 
     List<Item> findByItemNm(String itemNm);
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail); // 상품명과 상품 상세 설명 이용하여 조회
